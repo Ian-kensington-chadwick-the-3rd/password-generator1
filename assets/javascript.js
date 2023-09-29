ccc28087efd50b8cede54c18523cd2c0a5b39ffe
Ian Sills <iansills04@gmail.com>
2023-09-29 01:05:31 -0500

    added my js in increments to increase my pushes because my last repo was messed up because i copyied it over wrong from vs code

--- assets/javascript.js
+++ assets/javascript.js
@@ -35,3 +35,48 @@ function writePassword() {
     
    
    
+// the getPrompts function runs a list of ifs and returns all the values that chosen
+function getPrompts(){
+    choiceArr= []
+ 
+ // the pars int turns string to integer
+ characterLength = parseInt(prompt("How many characters do you want your password to be(8 - 128"));
+   
+    
+   if(isNaN(characterLength) || characterLength < 8 || characterLength > 128){
+     alert("pick a number between 8 - 128 digits. try again")
+     return false;
+   }  // concat combines arrays
+    if (confirm("Would you like uppercase letters in your password")){
+          choiceArr = choiceArr.concat(upperCaseArr);
+    }
+    if (confirm("Would you like lowercase letters in your password ")){
+        choiceArr = choiceArr.concat(lowerCaseArr);
+    }
+    if (confirm("Would you like uppercase letters in your password")){
+        choiceArr = choiceArr.concat(specialCharArr);
+    }
+    if (confirm("Would you like numbers in your password")){
+        choiceArr = choiceArr.concat(numericArr);
+    }
+    return true;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// 1.pull up promt to ask user if they want a password
+// 2.does user want numbers 
+// 3.does user want specialCharArr 
+// 4.does user want lowerCaseArr 
+// 5. does user want upperCaseArr 
+// 6.what if user pick none of the above 
+// 7. if everything is true generate password 
